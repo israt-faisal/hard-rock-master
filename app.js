dd6aa13353132baa10d70c9adc6ade57ea9bc566
israt faisal <israt.nsu141@gmail.com>
2021-06-16 22:56:10 -0400

    cords added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const songSearch = () => {
     fetch(`https://www.songsterr.com/a/ra/songs.json?pattern=${searchSong}`)
         .then(res => res.json())
         .then(data => titleSong(data))
+        
 }
 
 const titleSong =(songs) => {
@@ -11,6 +12,7 @@ const titleSong =(songs) => {
     const displaySong = document.getElementById('display-song');
     const my10Items = songs.slice(0, 10);
     my10Items.forEach(song => {
+        console.log(song)
 
         const div = document.createElement('div');
         div.className ="single-result row align-items-center my-3 p-3";
@@ -19,14 +21,21 @@ const titleSong =(songs) => {
         <div class="col-md-9">
         <h3 class="lyrics-name">${song.title}</h3>
         <p class="author lead">Album by <span>${song.artist.name}</span></p>
+        <p>Guiter Cords :<span> ${song.chordsPresent} </span></p>
     </div>
     <div class="col-md-3 text-md-right text-center">
-        <button class="btn btn-success">Get Lyrics</button>
+        <button onclick="getGuiterCords('${song.title}'+'${song.artist.name}')" id="getguitercords" class="btn btn-success">Get Guiter Cords</button>
     </div> `;
         displaySong.appendChild(div);
-    }
+    });
 
-   
-);
+}
+
+
+const getGuiterCords=(title, artist)=>{
+    // fetch(`http://www.songsterr.com/a/wa/bestMatchForQueryString?s=${title}&a=${artist}`)
+    fetch(`https://www.songsterr.com/a/wsa/${artist}-${title}`)
+    .then(res => res.json())
+    .then(data => console.log(data))
 
 }
